c1007e7ce821feba45d4861459d5549dd7b34932
sashikknox <sashikknox@gmail.com>
2022-04-29 16:48:49 +0300

    - add build script - add desktop file - add icons - add spec file

--- build.sh
+++ build.sh
@@ -1,3 +1,91 @@
 #!/bin/bash
+_pwd=`dirname $(readlink -e "$0")`
+pushd ${_pwd} &> /dev/null
 
-dependencies="libjpeg-turbo-devel"
+dependencies="libjpeg-turbo-devel wayland-devel wayland-egl-devel wayland-protocols-devel systemd-devel libGLESv2-devel"
+engine_exec="docker exec --user mersdk -w `pwd` aurora-os-build-engine"
+build_dir="build_rpm"
+
+if [[ "${engine_exec}" == *"aurora"* ]]; then
+    build_dir="build_aurora_rpm"
+fi
+
+build_root="`pwd`/${build_dir}"
+
+echo "Pack latest git cmmit to an archive: ${build_dir}/SOURCES/harbour-glmark2.tar.gz"
+rm -fr ${build_root}/{BUILD,SRPMS}
+mkdir -p ${build_root}/SOURCES
+
+git archive --output ${build_root}/SOURCES/harbour-glmark2.tar.gz HEAD
+
+if [[ "${engine_exec}" == *"aurora"* ]]; then
+    for each in key cert; do
+        if [ -f `pwd`/regular_${each}.pem ]; then 
+            echo "Файл ключа regular_${each}.pem найден: OK"
+            continue;
+        fi
+        echo -n "Скачиваем ключ regular_${each}.pem для подписи пактов под АврораОС: "
+        curl https://community.omprussia.ru/documentation/files/doc/regular_${each}.pem -o regular_${each}.pem &> /dev/null
+        if [ $? -eq 0 ]; then 
+            echo "OK"
+        else
+            echo "FAIL"
+            echo "Ошибка скачивания regular_${each}.pem: https://community.omprussia.ru/documentation/files/doc/regular_${each}.pem"
+            exit 1
+        fi
+    done
+fi
+
+sfdk_targets=`${engine_exec} sb2-config -l|grep -v default|grep armv7`
+
+for each in ${sfdk_targets}; do
+    target_arch=${each##*-}
+    echo "Build for '$each' target with '$target_arch' architecture"
+
+    if [ -d ${build_root}/BUILD ]; then 
+        rm -fr ${build_root}/BUILD
+    fi
+    mkdir -p ${build_root}/BUILD
+
+    target="${engine_exec} sb2 -t $each"
+
+    #install deps for current target
+    ${target} -R -m sdk-install zypper in -y ${dependencies}
+    
+    # build RPM for current target
+    ${target} rpmbuild --define "_topdir `pwd`/${build_dir}" --define "_arch $target_arch" -ba sailfish/sailfish.spec
+    if [ $? -ne 0 ] ; then 
+        echo "Build RPM for ${each} : FAIL"
+        continue; 
+    fi
+
+    # sign RPM packacge 
+    if [[ "${engine_exec}" == *"aurora"* ]]; then
+        echo -n "Signing RPMs: "
+        ${target} rpmsign-external sign --key `pwd`/regular_key.pem --cert `pwd`/regular_cert.pem ${build_root}/RPMS/${target_arch}/harbour-glmark2-1.*
+        if [ $? -ne 0 ] ; then 
+            echo "FAIL"
+            break; 
+        fi
+        echo "OK"
+        echo -n "Validate RPMs: "
+        validator_output=`${target} rpm-validator -p regular ${build_root}/RPMS/${target_arch}/harbour-glmark2-1.* 2>&1`
+        if [ $? -ne 0 ] ; then 
+            echo "FAIL"
+            echo "${validator_output}"
+            break; 
+        fi
+        echo "OK"
+    elif [[ "${engine_exec}" == "sfdk "* ]] ;then
+        echo -n "Validate RPM: "
+        sfdk config target=${each}
+        sfdk check ${build_root}/RPMS/${target_arch}/harbour-glmark2-1.*
+        if [ $? -ne 0 ] ; then 
+            echo "FAIL"
+            break;
+        fi
+        echo "OK"
+    fi
+done
+
+popd &> /dev/null
---
+++ sailfish/sailfish.spec
@@ -0,0 +1,66 @@
+Name:       harbour-glmark2
+Summary:    GLMark 2 
+Release:    1
+Version:    1.0.0
+Group:      Amusements/Games
+License:    GPLv3
+BuildArch:  %{_arch}
+URL:        https://github.com/savegame/sailfish-glmark2
+Source0:    %{name}.tar.gz
+
+BuildRequires: libjpeg-turbo-devel
+BuildRequires: wayland-devel
+BuildRequires: wayland-egl-devel
+BuildRequires: wayland-protocols-devel
+BuildRequires: systemd-devel
+BuildRequires: libGLESv2-devel
+BuildRequires: rsync
+
+%global build_folder %{_topdir}/BUILD/build
+
+%description
+glmark2 is developed by Alexandros Frantzis and Jesse Barker based on the
+original glmark benchmark by Ben Smith.
+SailfishOS port made by sashikknox <sashikknox@gmail.com>
+
+%prep
+echo "Unpack sources"
+# rm -fr %{_topdir}/BUILD
+# mkdir -p %{_topdir}/BUILD
+cd %{_topdir}/BUILD
+tar -xzf %{_topdir}/SOURCES/%{name}.tar.gz
+./waf configure \
+    --with-flavors=wayland-glesv2 \
+    --data-path=%{_datadir}/%{name}/data \
+    --prefix=/usr \
+    --no-debug
+
+%build
+cd %{_topdir}/BUILD/
+./waf build -j`nproc`
+# strip build/src/glmark2-es2-wayland
+
+
+%install
+rm -rf %{buildroot}
+mkdir -p %{buildroot}/usr/{bin,share/%{name}/data,share/applications}
+mkdir -p %{buildroot}/usr/share/icons/hicolor/{86x86,108x108,128x128,172x172}/apps/
+rsync -avP %{build_folder}/src/glmark2-es2-wayland %{buildroot}%{_bindir}/%{name}
+rsync -avP %{_topdir}/BUILD/android/assets/{models,textures,shaders} %{buildroot}%{_datadir}/%{name}/data/
+rsync -avP %{_topdir}/BUILD/sailfish/harbour-glmark2.desktop %{buildroot}%{_datadir}/applications/%{name}.desktop
+rsync -avP %{_topdir}/BUILD/sailfish/icon_86.png %{buildroot}/usr/share/icons/hicolor/86x86/apps/%{name}.png
+rsync -avP %{_topdir}/BUILD/sailfish/icon_108.png %{buildroot}/usr/share/icons/hicolor/108x108/apps/%{name}.png
+rsync -avP %{_topdir}/BUILD/sailfish/icon_128.png %{buildroot}/usr/share/icons/hicolor/128x128/apps/%{name}.png
+rsync -avP %{_topdir}/BUILD/sailfish/icon_172.png %{buildroot}/usr/share/icons/hicolor/172x172/apps/%{name}.png
+
+%files
+%defattr(-,root,root,-)
+%attr(755,root,root) %{_bindir}/%{name}
+%{_datadir}/icons/hicolor/*
+%dir %{_datadir}/%{name}/data
+%{_datadir}/%{name}/data/*
+%{_datadir}/applications/%{name}.desktop
+
+%changelog 
+* Fri Apr 29 2022 sashikknox <sashikknox@gmail.com>
+- fixes for SailfishOS wayland
